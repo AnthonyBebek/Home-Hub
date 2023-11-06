b9ddc32dac932b278995f083e13f2d50dadb5667
Fox2low <77130209+Fox2low@users.noreply.github.com>
2023-11-07 09:01:03 +1100

    Update actions.h

--- Hub-Code/actions.h
+++ Hub-Code/actions.h
@@ -230,13 +230,13 @@ const char actions_html[] PROGMEM = R"=====(
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>WASP Hub</title>
+  <title>Smart Home Hub</title>
 </head>
 
 <body>
   <header>
     <div class="logo">
-      <h1>WASP Hub</h1>
+      <h1>Smart Home Hub</h1>
     </div>
   </header>
   <!-- Nav Bar -->
@@ -252,6 +252,7 @@ const char actions_html[] PROGMEM = R"=====(
       <li><a href="hub">Hub</a></li>
       <li><a href="actions">Actions</a></li>
       <li><a href="network">Network</a></li>
+      <li><a href="graphs">Graphs</a></li>
     </ul>
     <div class="close-button" onclick="toggleNav()"></div>
   </nav>
@@ -285,8 +286,8 @@ const char actions_html[] PROGMEM = R"=====(
   </div>
 
   <footer>
-    <p>This website is running off a WASP Hub Device</p>
-    <p>&copy; 2023 WASP Hub. All rights reserved.</p>
+    <p>This website is running off a Home Hub Device</p>
+    <p>&copy; 2023 Home Hub. All rights reserved.</p>
   </footer>
   <script>
     function toggleNav() {
